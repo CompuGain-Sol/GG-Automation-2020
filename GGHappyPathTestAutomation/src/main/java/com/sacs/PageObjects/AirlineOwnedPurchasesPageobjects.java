cf2abe023b085f7de6f99bf11f1ad593ce639073
vivek <vivek.kundarapu@compugain.com>
2020-02-17 18:56:15 +0530

    Removed the code

--- GGHappyPathTestAutomation/src/main/java/com/sacs/PageObjects/AirlineOwnedPurchasesPageobjects.java
+++
@@ -1,7 +0,0 @@
-package com.sacs.PageObjects;
-
-import org.openqa.selenium.By;
-
-public interface AirlineOwnedPurchasesPageobjects {
-	By airlineownedpurchaseposubmittedtext=By.xpath("//tr[@class='articlerow k-row']/td[4]/div");
-}
